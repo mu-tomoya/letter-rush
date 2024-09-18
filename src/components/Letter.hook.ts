4d75290e6092d3a0633e78c5649a32ebb929a75a
mu_tomoya <muton.tm.714@gmail.com>
2024-09-18 16:04:46 +0900

    localStorageのデータの持ち方の変更と、ページ削除ボタンの設置

--- src/components/Letter.hook.ts
+++ src/components/Letter.hook.ts
@@ -2,15 +2,17 @@ import { ChangeEvent, KeyboardEvent, useCallback, useState } from "react";
 
 export const useLetterHook = () => {
   const [pages, setPages] = useState<string[]>(() => {
-    const savedPages = localStorage.getItem("a5Pages");
-    return savedPages ? JSON.parse(savedPages) : [""];
+    const a5Pages = localStorage.getItem("a5Pages");
+    if (a5Pages) {
+      return JSON.parse(a5Pages).split("\f");
+    }
+    return [""];
   });
 
   const [lineColor, setLineColor] = useState<string>("#333");
 
   const handleAddPage = useCallback((): void => {
     setPages([...pages, ""]);
-    localStorage.setItem("a5Pages", JSON.stringify(pages));
   }, [pages]);
 
   const handleChange = useCallback(
@@ -18,7 +20,8 @@ export const useLetterHook = () => {
       const newPages = [...pages];
       newPages[index] = e.target.value;
       setPages(newPages);
-      localStorage.setItem("a5Pages", JSON.stringify(newPages));
+      const a5Pages = newPages.join("\f");
+      localStorage.setItem("a5Pages", JSON.stringify(a5Pages));
     },
     [pages]
   );
@@ -40,12 +43,23 @@ export const useLetterHook = () => {
     [pages]
   );
 
+  const handleRemovePage = useCallback(
+    (index: number): void => {
+      const newPages = [...pages];
+      newPages.splice(index, 1);
+      setPages(newPages);
+      localStorage.setItem("a5Pages", JSON.stringify(newPages.join("\f")));
+    },
+    [pages]
+  );
+
   return {
     pages,
     handleAddPage,
     handleChange,
     handleReset,
     handleKeyDown,
+    handleRemovePage,
     lineColor,
     setLineColor,
   };
--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,5 +1,7 @@
+import React from "react";
 import { useLetterHook } from "./Letter.hook";
 import useSettings from "./useSetting";
+import { DeleteButton } from "./DeleteButton";
 
 const A5LetterEditor: React.FC = () => {
   const {
@@ -10,12 +12,15 @@ const A5LetterEditor: React.FC = () => {
     fontFamily,
     letterSpacing,
   } = useSettings();
-  const { pages, handleAddPage, handleChange, handleReset, handleKeyDown } =
+
+  const { pages, handleAddPage, handleReset, handleChange, handleRemovePage } =
     useLetterHook();
   return (
     <form className="a5-editor-container">
       <div className="button-group">
-        <button onClick={() => window.print()}>印刷</button>
+        <button className="button" type="button" onClick={() => window.print()}>
+          印刷
+        </button>
       </div>
       {pages.map((pageText, index) => (
         <div
@@ -30,21 +35,21 @@ const A5LetterEditor: React.FC = () => {
             ["--letter-spacing" as any]: `${letterSpacing}px`,
           }}
         >
+          <DeleteButton onClick={() => handleRemovePage(index)} />
           <textarea
             aria-label={`ページ${index + 1}`}
             value={pageText}
             cols={27}
             rows={17}
             onChange={(e) => handleChange(index, e)}
-            onKeyDown={(e) => handleKeyDown(index, e)}
           />
         </div>
       ))}
       <div className="button-group">
-        <button type="reset" onClick={handleReset}>
+        <button className="button" type="reset" onClick={handleReset}>
           リセット
         </button>
-        <button type="button" onClick={handleAddPage}>
+        <button className="button" type="button" onClick={handleAddPage}>
           ページ追加
         </button>
       </div>
